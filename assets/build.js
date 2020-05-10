79f8d13c95bf0930540bdbc36933a369e152808b
Jakob Fix <jakob@jfix.com>
2020-05-10 15:03:00 +0200

    Add episode handling
    For each episode create an episode menu which features some technical
    data and, more importantly, the summary of the episode, as supplied in
    the `.txt` files for each one.
    
    Optimised the code for many of the repetitive tasks by using an Object
    containing the three states and the corresponding colours.
    
    Also reduced the `ffmpeg` output by adding some flags to hide the banner
    and reduce the log level. Finally, added a small reminder of what to do
    next.

--- assets/build.js
+++ assets/build.js
@@ -8,6 +8,7 @@ moment.locale('de')
 
 const dvdNumber = '1'
 const inDir = path.resolve(__dirname, `../dvds/DVD-${dvdNumber.padStart(2, '0')}`)
+const assetsInDir = `${inDir}/assets`
 const assetsDir = __dirname
 const outDir = inDir
 const outFile = path.resolve(outDir, 'generate.sh')
@@ -20,50 +21,78 @@ const cleanUp = () => {
     stream.write(`rm ${inDir}/assets/*\n`)
     stream.write(`rm ${inDir}/dvd.xml\n`)
 }
+const states = {
+    'normal': 'white',
+    'active': 'red',
+    'selected': 'green'
+}
 
+// three transparent background images to merge with the episode background
+const createGenericEpisodeBackground = () => {
+    Object.keys(states).forEach((state) => {
+        const colour = states[state]
+        stream.write(`convert -background none -bordercolor ${colour} -border 2 -fill ${colour} -pointsize 20 label:" Zurück zum Menü " PNG8:${assetsInDir}/back-to-menu-${state}.png\n`)
+        stream.write(`convert -background none -bordercolor ${colour} -border 2 -fill ${colour} -pointsize 20 label:" Episode abspielen " PNG8:${assetsInDir}/play-episode-${state}.png\n`)
+        stream.write(`convert -size 720x576 xc:none ${assetsInDir}/back-to-menu-${state}.png -geometry +530+270 -composite ${assetsInDir}/play-episode-${state}.png -geometry +530+225 -composite PNG8:${assetsInDir}/episode-button-background-${state}.png\n`)
+    })
+}
+
+const handleEpisode = (episodeId) => {
+    // make an image of the episode text description file
+    stream.write(`sed -n -e 1,10p -e 17,50p ${inDir}/${episodeId}.txt | convert -background black -size 680x -fill white -border 20 -bordercolor black -font Courier -pointsize 16 caption:@- ${assetsInDir}/${episodeId}-backdrop.png\n`)
+    
+    // add the "Chronik der Wende" logo to the top right corner
+    stream.write(`convert ${assetsInDir}/${episodeId}-backdrop.png ${assetsDir}/episode-logo.png -geometry +540+20 -composite ${assetsInDir}/${episodeId}-description.png\n`)
+    
+    // take the image and convert it to a video
+    stream.write(`ffmpeg -loglevel warning -hide_banner -y -loop 1 -i ${assetsInDir}/${episodeId}-description.png -t 5 -aspect 16:9 -target pal-dvd ${assetsInDir}/${episodeId}-menu1.mpeg\n`)
+    // add silent audio to the video
+    stream.write(`ffmpeg -loglevel warning -hide_banner -y -i ${assetsDir}/menu-audio.mp2 -i ${assetsInDir}/${episodeId}-menu1.mpeg -aspect 16:9 -target pal-dvd ${assetsInDir}/${episodeId}-menu2.mpeg\n`)
+    // mux 
+    stream.write(`spumux ${assetsDir}/episode-button-template.xml < ${assetsInDir}/${episodeId}-menu2.mpeg > ${assetsInDir}/${episodeId}-menu.mpeg\n`)
+
+    return `<titleset><menus><pgc><button>jump title 1;</button><button>jump vmgm menu 1;</button><vob file="assets/${episodeId}-menu.mpeg" pause="inf"/></pgc></menus><titles><pgc><post>call menu;</post><vob file="${episodeId}.mpeg"/></pgc></titles></titleset>`
+}
 // ---------------------------------------------------------
 
 const readFiles = (e, files) => {
     let firstDate, lastDate, currentDate, titlesets = ''
 
     cleanUp()
+    createGenericEpisodeBackground()
+    stream.write(`export VIDEO_FORMAT=PAL\n`)
 
     files.forEach((f, i) => {
-
-        const filename = path.basename(f)
-        console.log(`==> ${filename}`)
-        const date = moment(filename.match(/_(\d{4}-\d{2}-\d{2})-/)[1])
+        const episodeId = path.basename(f, '.mpeg')     
+        console.log(`==> ${episodeId}`)
+        const date = moment(episodeId.match(/_(\d{4}-\d{2}-\d{2})-/)[1])
         if (i === 0) firstDate = date.format('D. MMMM')
         currentDate = date.format('dddd, D. MMM')
 
-        stream.write(`convert -background none -bordercolor white -border 2 -fill white -pointsize 20 label:"${currentDate}" PNG8:${inDir}/assets/button-normal-${i+1}.png\n`)
-        stream.write(`convert -background none -bordercolor green -border 2 -fill green -pointsize 20 label:"${currentDate}" PNG8:${inDir}/assets/button-active-${i+1}.png\n`)
-        stream.write(`convert -background none -bordercolor red -border 2 -fill red -pointsize 20 label:"${currentDate}" PNG8:${inDir}/assets/button-selected-${i+1}.png\n`)
-
-        // TODO: create images then videos from the text files
-        //  sed -n -e 1,10p -e 17,50p dvds/DVD-01/Chronik_der_Wende_1989-10-07-1097577097.txt |convert -background blue -size 680x -fill white -border 20 -bordercolor blue -font Courier -pointsize 16 caption:@- test.png
-
+        Object.keys(states).forEach((state) => {
+            const colour = states[state]
+            stream.write(`convert -background none -bordercolor ${colour} -border 2 -fill ${colour} -pointsize 20 label:" ${currentDate} " PNG8:${assetsInDir}/button-${state}-${i+1}.png\n`)
+        })
+        
         if (i + 1 === files.length) {
             lastDate = date.format('D. MMMM YYYY')
-            stream.write(`convert ${assetsDir}/bg-template.png -pointsize 30 -draw "gravity south fill white text 0,90 'DVD ${dvdNumber}      ${firstDate} bis ${lastDate}'" PNG8:${inDir}/assets/dvd-label.png\n`)
+            stream.write(`convert ${assetsDir}/bg-template.png -pointsize 30 -draw "gravity south fill white text 0,90 'DVD ${dvdNumber}      ${firstDate} bis ${lastDate}'" PNG8:${assetsInDir}/dvd-label.png\n`)
         }
-        const titleset = `<titleset><titles><pgc><post>call vmgm menu 1;</post><vob file="${filename}"/></pgc></titles></titleset>`
-        titlesets += titleset
+        titlesets += handleEpisode(episodeId, i)
+
     })
-    
     stream.write(`cp ${assetsDir}/dvd-template.xml ${inDir}/dvd.xml\n`)
     stream.write(`sed -i '' -e 's#TITLESETS#${titlesets}#' ${inDir}/dvd.xml\n`)
 
-    stream.write(`convert -size 720x576 xc:none assets/button-normal-1.png -geometry +40+80 -composite assets/button-normal-2.png -geometry +40+130 -composite assets/button-normal-3.png -geometry +40+185 -composite assets/button-normal-4.png -geometry +40+240 -composite assets/button-normal-5.png -geometry +40+295 -composite assets/button-normal-6.png -geometry +510+80 -composite assets/button-normal-7.png -geometry +510+130 -composite assets/button-normal-8.png -geometry +510+185 -composite assets/button-normal-9.png -geometry +510+240 -composite assets/button-normal-10.png -geometry +510+295 -composite PNG8:assets/background-normal.png\n`)
+    Object.keys(states).forEach((state) => {
+        stream.write(`convert -size 720x576 xc:none ${assetsInDir}/button-${state}-1.png -geometry +40+80 -composite ${assetsInDir}/button-${state}-2.png -geometry +40+130 -composite ${assetsInDir}/button-${state}-3.png -geometry +40+185 -composite ${assetsInDir}/button-${state}-4.png -geometry +40+240 -composite ${assetsInDir}/button-${state}-5.png -geometry +40+295 -composite ${assetsInDir}/button-${state}-6.png -geometry +510+80 -composite ${assetsInDir}/button-${state}-7.png -geometry +510+130 -composite ${assetsInDir}/button-${state}-8.png -geometry +510+185 -composite ${assetsInDir}/button-${state}-9.png -geometry +510+240 -composite ${assetsInDir}/button-${state}-10.png -geometry +510+295 -composite PNG8:${assetsInDir}/background-${state}.png\n`)
+    })
 
-    stream.write(`convert -size 720x576 xc:none assets/button-active-1.png -geometry +40+80 -composite assets/button-active-2.png -geometry +40+130 -composite assets/button-active-3.png -geometry +40+185 -composite assets/button-active-4.png -geometry +40+240 -composite assets/button-active-5.png -geometry +40+295 -composite assets/button-active-6.png -geometry +510+80 -composite assets/button-active-7.png -geometry +510+130 -composite assets/button-active-8.png -geometry +510+185 -composite assets/button-active-9.png -geometry +510+240 -composite assets/button-active-10.png -geometry +510+295 -composite PNG8:assets/background-active.png\n`)
-
-    stream.write(`convert -size 720x576 xc:none assets/button-selected-1.png -geometry +40+80 -composite assets/button-selected-2.png -geometry +40+130 -composite assets/button-selected-3.png -geometry +40+185 -composite assets/button-selected-4.png -geometry +40+240 -composite assets/button-selected-5.png -geometry +40+295 -composite assets/button-selected-6.png -geometry +510+80 -composite assets/button-selected-7.png -geometry +510+130 -composite assets/button-selected-8.png -geometry +510+185 -composite assets/button-selected-9.png -geometry +510+240 -composite assets/button-selected-10.png -geometry +510+295 -composite PNG8:assets/background-selected.png\n`)
-
-    stream.write(`ffmpeg -y -loop 1 -i assets/dvd-label.png -t 5 -aspect 16:9 -target pal-dvd assets/menu-video.mpeg\n`)
-    stream.write(`ffmpeg -y -i ${assetsDir}/menu-audio.mp2 -i assets/menu-video.mpeg -aspect 16:9 -target pal-dvd assets/menu.mpeg\n`)
+    stream.write(`ffmpeg -loglevel warning -hide_banner -y -loop 1 -i assets/dvd-label.png -t 5 -aspect 16:9 -target pal-dvd assets/menu-video.mpeg\n`)
+    stream.write(`ffmpeg -loglevel warning -hide_banner -y -i ${assetsDir}/menu-audio.mp2 -i assets/menu-video.mpeg -aspect 16:9 -target pal-dvd assets/menu.mpeg\n`)
     stream.write(`export VIDEO_FORMAT=PAL\n`)
-    stream.write(`spumux ${assetsDir}/menu-buttons-template.xml < assets/menu.mpeg > assets/menu-final.mpeg`)
+    stream.write(`spumux ${assetsDir}/menu-buttons-template.xml < assets/menu.mpeg > assets/menu-final.mpeg\n`)
+    stream.write(`\n\necho 'Now run: dvdauthor -x dvd.xml'\n\n`)
 }
 
 glob(`${inDir}/*.mpeg`, {}, readFiles)
